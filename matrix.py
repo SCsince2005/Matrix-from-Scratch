209ab9008bb0b462f14aa4aa738355e4a291632c
SCsince2005 <scsince2005@gmail.com>
2025-07-14 20:41:38 +0530

    Added determinent for 2x2 & 3x3 and scalar multiplication

--- matrix.py
+++ matrix.py
@@ -54,9 +54,40 @@ class Matrix:
             trans.append(row) 
         return Matrix(trans)
 
+    def determinent(self):
+        if all(len(row) == len(self.data) for row in self.data):
+            if len(self.data)==2:
+                det=(self.data[0][0]*self.data[1][1])-(self.data[0][1]*self.data[1][0])
+                return det
+            elif len(self.data)==3:
+                a = self.data[0][0]
+                b = self.data[0][1]
+                c = self.data[0][2]
+                d = self.data[1][0]
+                e = self.data[1][1]
+                f = self.data[1][2]
+                g = self.data[2][0]
+                h = self.data[2][1]
+                i = self.data[2][2]
+                det = (
+                    a * (e * i - f * h)
+                    - b * (d * i - f * g)
+                    + c * (d * h - e * g)
+                )
+                return det
+        else:
+            print("Not a square matrix!!!")
+            return None
+
+    def scalarMultiply(self,n):
+        for row in self.data:
+            for i in range(len(row)):
+                row[i]=n*row[i]
+        return self
+        
 
 # Basic Matrix Test
-m1=Matrix([[1, 2, 3],[4, 5, 6],[1, 2, 3]])
+m1=Matrix([[1, 2, 3],[4, 5, 6],[1, 2, 5]])
 m2=Matrix([[7, 8, 9],[10, 11, 12], [4, 5, 6]])
 
 m1.add(m2).display()
@@ -66,3 +97,6 @@ print()
 m1.multiply(m2).display()
 print()
 m1.transpose().display()
+print()
+print(m1.determinent())
+m1.scalarMultiply(2).display()
